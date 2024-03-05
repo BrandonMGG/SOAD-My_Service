8986a329298b30698fed7b4c6d03744307c83a0a
rijegaro287 <gatgens48@gmail.com>
2024-03-04 21:45:52 -0600

    Delete unnecessary comments

--- src/swagger-config.js
+++ src/swagger-config.js
@@ -22,42 +22,6 @@ const swaggerOptions = {
           }
         }
       },
-      /**
- * @swagger
- * /recommendation:
- *  post:
- *    tags:
- *      - Recommendations
- *    summary: Obtener recomendaciones de comida
- *    description: Permite obtener recomendaciones de comida basado en el input del usuario.
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            type: array
- *            items:
- *              type: object
- *              properties:
- *                tipo: 
- *                  type: string
- *                input: 
- *                  type: string
- *                endpoint: 
- *                  type: string
- *    responses:
- *      '200':
- *         description: Successfully retrieved recommendations for the given input.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                recomendaciones:
- *                  type: array
- *                  items:
- *                    type: string
- */
       '/recommendation': {
         post: {
           tags: ['Recommendation'],
